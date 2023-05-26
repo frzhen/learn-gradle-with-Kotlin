10e52cc6e9dcb5a5c3e0fcc7d848390ecb1ac867
Fred Zhen <fred.zhen@gmail.com>
2023-05-26 21:15:55 +0800

    Created test and modify dependencies for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,26 @@
+import groovy.xml.dom.DOMCategory.attributes
+import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
+
 plugins {
     java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "guru.ysy.languageapp.SayHello"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
